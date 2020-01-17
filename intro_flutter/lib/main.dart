28d20e75028ddedbf1c84ee48aa680aeb6b4cf81
Patrick <patrickaduarte>
2020-01-17 16:31:49 -0300

    alterações no main

--- intro_flutter/lib/main.dart
+++ intro_flutter/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'ui/welcome.dart';
+//import 'ui/welcome.dart';
+import 'ui/coluna.dart';
 
 void main() {
   runApp(
     new MaterialApp(
       title: "Hello World",
-      home: new Welcome(),
+      home: new Coluna(),
     ));
 }
