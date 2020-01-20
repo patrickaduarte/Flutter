a2e8d56985934ef3028ffe51c6a5355583fb599e
Patrick <patrickaduarte>
2020-01-20 15:58:40 -0300

    Projeto Tycoon completo com operador ternario comentado

--- tycoon/lib/ui/home.dart
+++ tycoon/lib/ui/home.dart
@@ -1,49 +1,70 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+class _HomeState extends State<Home> {
+  int _contadorGrana = 0;
+  MaterialColor _colorMoney = Colors.red;
+  void _moreMoney(){
+
+    setState(() {
+      _contadorGrana += 1000;//Atualiza os valores da tela.
+      if (_contadorGrana >= 10000){
+        _colorMoney = Colors.green;
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         title: Text("Tycoon"),
         backgroundColor: Colors.lightGreen,
-        actions: <Widget>[
-      ],),
-      backgroundColor: Colors.white,
+        ),
+
       body: Container(
-        alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text ("Be rich!",
-            style: TextStyle(
-              fontSize: 30.0,
-              //fontWeight: FontWeight.w700,
-              color: Colors.grey
-              ),
-            ),
-            Text ("@ 9300",
-            style: TextStyle(
-              fontSize: 40.0,
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-              ),
-            ),
-            FlatButton(
-              onPressed: () => debugPrint("Teste"),
-              color: Colors.lightGreen,
-              child: Text('More Money!',
+            Center(
+              child: Text("Be rich!",
               style: TextStyle(
-                fontSize: 15.0,
-                color: Colors.white 
+                color: Colors.grey,
+                fontWeight: FontWeight.w400,
+                fontSize: 30.0
                 ),
               ),
             ),
+            Expanded(
+              child: Center(
+                child: Text("\$ $_contadorGrana",
+                  style: TextStyle(
+                    fontSize: 45.6,
+                    color: _colorMoney,
+                    //_contadorGrana >= 10000 ? Colors.green : Colors.amber,
+                    fontWeight: FontWeight.w700,
+                  ),
+                )
+              ,)
+            ,),
+            Expanded(
+              child: Center(
+                child: FlatButton(
+                  onPressed: _moreMoney,
+                  child: Text("More money!",
+                    style: TextStyle(
+                      fontSize: 19.9
+                    ),
+                  ),
+                  color: Colors.green,
+                  textColor: Colors.white,
+                ),
+              ),
+            )
           ],
-        ),
+        )
       ),
-
     );
   }
 }
