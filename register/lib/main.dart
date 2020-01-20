7df1817f60663c79b3dfffb870b0e6d6d2f8e5dd
Patrick <patrickaduarte>
2020-01-20 16:58:54 -0300

    Layout de tela de login

---
+++ register/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'ui/register.dart';
+
+
+void main() => runApp(
+  new MaterialApp(
+    home: new Register(),
+  ));
---
+++ register/lib/ui/register.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register",),
+        backgroundColor: Colors.deepOrange,
+      ),    
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: ListView(
+          children: <Widget>[
+            Image.asset("assets/face.png",
+              width: 90.0,
+              height: 90.0,
+              color: Colors.amber,
+            ),
+            Container(
+              width: double.infinity,
+              color:  Colors.amber,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Nome',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(4.5),
+                        ),
+                        //icon: Icon(Icons.person)
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Senha',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(4.5),
+                        ),
+                        //icon: Icon(Icons.person)
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ), 
+    );
+  }
+}
