b408ba00b08179875be9b94af64c23f9d4f00c54
Patrick <patrickaduarte>
2020-01-22 10:27:21 -0300

    Introdution of JSON

---
+++ json_analyze/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {    
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JSON"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future<List> getJson() async {
+  String url = 'https://jsonplaceholder.typicode.com/comments';
+
+  http.Response response = await http.get(url);
+  //status code == 200 = OK
+  if (response.statusCode == 200){
+    return json.decode(response.body);
+  }
+  else {
+    throw Exception('Fail');
+  }
+}
